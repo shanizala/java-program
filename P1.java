e1e5381d65d1d27431d169216a02b1bbe2bd82b5
shanizala <132726720+shanizala@users.noreply.github.com>
2025-06-15 00:12:14 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class P1
+{
+    public static void main(String [] args)
+    {
+
+        System.out.println("Shani zalavadiya ");
+
+        
+        Scanner scanner = new Scanner (System.in);
+
+        int i;
+    
+        
+        for(i=0;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
